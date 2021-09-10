0e43b3f2159b164e7375c86b596f83394b02e8f2
unknown <ipriyanshugautam@gmail.com>
2021-09-10 10:47:53 +0530

    Updates

---
+++ precache-manifest.8cd686bdb08492a365d0401baa538bc7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b725bd4008b55ffb726bf410df3384b",
+    "url": "/Keeper_TakeNotes/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/Keeper_TakeNotes/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "1699a401a6be9f7b6795",
+    "url": "/Keeper_TakeNotes/static/js/main.6742f93f.chunk.js"
+  },
+  {
+    "revision": "fc9ad985ef8710584251",
+    "url": "/Keeper_TakeNotes/static/js/runtime-main.a3cddfd0.js"
+  }
+]);
